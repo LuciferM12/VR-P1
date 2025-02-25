1164ddf3924cea84cffb30a10a3ea320818d9543
LuciferM121 <rojasrodriguezmartinomar@gmail.com>
2025-02-25 02:08:59 -0600

    agrega mas prefabs y random spawner

--- VR-P1/Assets/Scripts/CactusSpawner.cs
+++ VR-P1/Assets/Scripts/CactusSpawner.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class CactusSpawner : MonoBehaviour
 {
-    public GameObject cactusPrefab;
+    public GameObject[] cactusPrefabs;
     public float spawnInterval = 2f;
-    public Transform imageTarget; // Referencia al Image Target
-
+    public Transform imageTarget; 
     void Start()
     {
         InvokeRepeating("SpawnCactus", 1f, spawnInterval);
@@ -15,8 +14,10 @@ public class CactusSpawner : MonoBehaviour
 
     void SpawnCactus()
     {
-        if (imageTarget != null)
+        if (imageTarget != null && cactusPrefabs.Length > 0)
         {
+
+            GameObject cactusPrefab = cactusPrefabs[Random.Range(0, cactusPrefabs.Length)];
             Vector3 spawnPosition = imageTarget.TransformPoint(new Vector3(1, 0, 0));
             Instantiate(cactusPrefab, spawnPosition, Quaternion.identity, imageTarget);
         }
